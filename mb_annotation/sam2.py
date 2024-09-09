9be377ebe655e5c8424bd3ce2af5c9aee845ee05
Malav <malav.b93@gmail.com>
2024-09-09 02:55:42 +0100

    updated version.py to 1.0.13

--- mb_annotation/sam2.py
+++ mb_annotation/sam2.py
@@ -2,9 +2,15 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 from sam2.build_sam import build_sam2
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 import cv2
+from sam2.build_sam import build_sam2_video_predictor
+import os
+from os import listdir
+from os.path import isfile, join
+
 
 __all__ = ["show_anns","get_mask_generator","get_mask_for_bbox","get_all_masks"]
 
@@ -93,7 +99,6 @@ def get_mask_for_bbox(image_path,bbox_value,sam2_checkpoint,model_cfg,device='cp
     print('Getting mask')
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image_height,image_width,_ = image.shape
     mask_generator = get_mask_generator(sam2_checkpoint,model_cfg,device)
     mask_full = mask_generator.generate(image)
     if show_full:
@@ -132,3 +137,119 @@ def get_all_masks(image_path,sam2_checkpoint,model_cfg,device='cpu',*kwargs):
     mask_full = mask_generator.generate(image)
     print('Getting final mask')
     return mask_full
+
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    """
+    Display a mask on a given axis.
+    Args:
+        mask (np.ndarray): The mask to be displayed.
+        ax (matplotlib.axes.Axes): The axis on which to display the mask.
+        obj_id (int, optional): Object ID for color selection. Defaults to None.
+        random_color (bool, optional): If True, use a random color. Defaults to False.
+    Returns:
+        None
+    """
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+def show_points(coords, labels, ax, marker_size=200):
+    """
+    Display points on a given axis.
+    Args:
+        coords (np.ndarray): Array of point coordinates.
+        labels (np.ndarray): Array of point labels (1 for positive, 0 for negative).
+        ax (matplotlib.axes.Axes): The axis on which to display the points.
+        marker_size (int, optional): Size of the markers. Defaults to 200.
+    Returns:
+        None
+    """
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+def show_box(box, ax):
+    """
+    Display a bounding box on a given axis.
+    Args:
+        box (list or np.ndarray): The bounding box coordinates [x0, y0, x1, y1].
+        ax (matplotlib.axes.Axes): The axis on which to display the box.
+    Returns:
+        None
+    """
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+
+class video_predictor:
+    """
+    Segmentation anything 2 video predictor
+    Args:
+
+    Returns:
+        None
+    """
+    def __init__(self,model_cfg,sam2_checkpoint,device='cpu') -> None:
+        self.predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=device)
+
+    def inference(self,video_image_folder):
+        """
+        Initializing video from path of video frames folder
+        """
+        self.video_image_folder=video_image_folder
+        self.frame_names = [f for f in listdir(self.video_image_folder) if isfile(join(self.video_image_folder, f))]
+        self.frame_names = sorted(self.frame_names)
+        self.joined_frame_names = [self.video_image_folder+'/'+self.frame_names[i] for i in range(len(self.frame_names))]
+        self.inference = self.predictor.init_state(video_path=video_image_folder)
+
+    def reset_state(self):
+        """
+        Reseting state to base
+        """
+        self.predictor.reset_state(self.inference)
+    
+    def predict(self,bbox:list[list]= None,points: list[list]=None,labels: list=None,frame_idx=0,show=True,image_loc=None,**kwargs):
+        """
+        Getting prediction from bounding box. Can add points to determine the location of segmentation
+        """
+        ann_obj_id=1
+        if points and labels:
+            points = np.array(points,dtype=np.float32)
+            labels = np.array(labels,np.int32)
+            prompts={}
+            prompts[ann_obj_id] = points, labels
+        if bbox:
+            bbox = np.array(bbox,dtype=np.float32)
+
+        assert len(points)==len(labels),"Length of points not equal to length of labels"
+
+        _, out_obj_ids, out_mask_logits = self.predictor.add_new_points_or_box(
+                                            inference_state=self.inference_state,
+                                            frame_idx=frame_idx,
+                                            obj_id=ann_obj_id,
+                                            points=points,
+                                            labels=labels,
+                                            box=bbox,**kwargs)
+
+        if show:
+            plt.figure(figsize=(9, 6))
+            plt.title(f"frame {frame_idx}")
+            if image_loc==None:
+                image_loc =self.joined_frame_names[0]
+            plt.imshow(Image.open(image_loc))
+            if points and labels:
+                show_points(points, labels, plt.gca())
+            for i, out_obj_id in enumerate(out_obj_ids):
+                if points and labels:
+                    show_points(*prompts[out_obj_id], plt.gca())
+                show_mask((out_mask_logits[i] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_id)
+
+
