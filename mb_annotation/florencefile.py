b0f74809a803e885c633203b09c05acc56222892
Malav <malav.b93@gmail.com>
2024-09-23 03:07:29 +0100

    updated version.py to 1.0.28

--- mb_annotation/florencefile.py
+++ mb_annotation/florencefile.py
@@ -6,6 +6,18 @@ from transformers import AutoProcessor, AutoModelForCausalLM
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
+import pandas as pd
+from transformers import (
+    AdamW,
+    AutoModelForCausalLM,
+    AutoProcessor,
+    get_scheduler
+)
+from tqdm import tqdm
+from typing import List, Dict, Any, Tuple, Generator
+from peft import LoraConfig, get_peft_model
+from torch.utils.data import Dataset, DataLoader
+from functools import partial
 
 __all__ = ["florence_model"]
 
@@ -206,3 +218,152 @@ class florence_model:
         if save_path is not None:
             image.save(save_path)
         return image
+    
+    def load_model(self,model_path:str):
+        """
+        Function to load model
+        Args:
+            model_path (str): Model path
+        Returns:
+            None
+        """
+        self.model = AutoModelForCausalLM.from_pretrained(model_path).to(self.device)
+
+    def load_collate(self):
+        """
+        Function to load collate
+        Args:
+            None
+        Returns:
+            None
+        """
+        partial_collate = partial(my_collate_fn, processor=self.processor)
+        return partial_collate
+
+class load_florence_dataset:
+    """
+    Requires csv file path.
+    CSV file should have:
+        image path (image_path) ,
+        boundingbox or suffix (bbox in the format [xmin, ymin, xmax, ymax] -should be in order of labels.) -list[list], 
+        prefix (if not mentioned, default <OD> will be used), 
+        labels or suffix (labels will be used as suffix after converting. if suffix is used, please make it in proper format) -list[list]
+        train_type : train or validation (if not mentioned, default random 80:20 will be used)
+        
+    Check the test file for more details. (./test_data/florence_test.csv)   
+    Args:
+        csv_file_path (str): Path to the csv file
+    Returns:
+        None
+    """
+
+    def __init__(self,csv_file_path) -> None:
+        self.df = pd.read_csv(csv_file_path)
+        if 'bbox' not in self.df.columns and 'suffix' not in self.df.columns:
+            raise ValueError("CSV file should have 'bbox' column or 'suffix' columns")
+        if 'labels' not in self.df.columns and 'suffix' not in self.df.columns:
+            raise ValueError("CSV file should have 'labels' or 'suffix' columns")
+        if 'prefix' not in self.df.columns:
+            self.df['prefix'] = '<OD>'
+        if 'prefix' in self.df.columns:
+            prefix_type_list =  ['<CAPTION>','<DETAILED_CAPTION>','<MORE_DETAILED_CAPTION>','<OCR>','<OCR_WITH_REGION>',
+                                 '<OD>','<REGION_PROPOSAL>','<DENSE_REGION_PROPOSAL>','<REGION_TO_SEGMENTATION>',
+                                 '<REGION_TO_CATOGORY>','<REGION_TO_DESCRIPTION>',
+                                 '<PHRASE_GROUNDING>','<OPEN_VOCABULARY_DETECTION>','<REFERRING_EXPRESSION_SEGMENTATION>']
+            prefix_u = list(self.df['prefix'].unique())
+            assert len(prefix_u)==1,'Check prefix - multiple values'
+            assert type(prefix_type_list.index(prefix_u[0]))==int,'Check prefix value. Not in index'
+        
+        if 'train_type' not in self.df.columns:
+            self.df['train_type'] = 'train'
+            val_indices = np.random.choice(self.df.index, size=int(len(self.df) * 0.2), replace=False)
+            self.df.loc[val_indices, 'train_type'] = 'validation'
+
+        final_data = []
+        if 'suffix' not in self.df.columns:
+            self.df['suffix'] = None
+            temp_dict={}
+            prefix_val = self.df.prefix.iloc[0]
+            for i, row in self.df.iterrows():
+                if isinstance(row['bbox'], str):
+                    bbox_list = eval(row['bbox'])
+                else:
+                    bbox_list = row['bbox']
+                    
+                if isinstance(row['labels'], str):
+                    try:
+                        labels_list = eval(row['labels'])
+                    except:
+                        labels_list = [row['labels']]
+                else:
+                    labels_list = row['labels']
+                assert len(bbox_list)==len(labels_list),f'BBox list and labels list not equal in row {i}'
+                temp_str = ""
+                for j in range(len(bbox_list)):
+                    if isinstance(bbox_list[j],str):
+                        bbox_list[j] = eval(bbox_list[j])
+                    temp_str = temp_str+ f"{labels_list[j]}<loc_{int(bbox_list[j][0])}><loc_{int(bbox_list[j][1])}><loc_{int(bbox_list[j][2])}><loc_{int(bbox_list[j][3])}>"
+                self.df.at[i, 'suffix'] = temp_str
+                temp_dict['image'] = row['image_path']
+                temp_dict['prefix'] = prefix_val
+                temp_dict['suffix'] = temp_str
+                final_data.append(temp_dict)
+        self.final_csv = pd.DataFrame(final_data)
+        self.final_csv.to_csv('./final_data_florence.csv',index=False)
+        print(f'final data example : {final_data[0]}')
+        print(f'final csv example: {self.final_csv.head(2)}')
+            
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        return self.final_csv.iloc[idx]
+
+class dataset_data(Dataset):
+    def __init__(self,df) -> None:
+        self.df = df
+    
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        # row_data = self.final_csv[self.final_csv.index==idx] ## check how to seperate train/val before goin to this step
+        image_path = self.df.iloc[idx]['image_path']
+        prefix = self.df.iloc[idx]['prefix']
+        suffix = self.df.iloc[idx]['suffix']
+        try:
+            image = Image.open(image_path)
+        except:
+            print(f"Error opening image: {image_path}")
+        return prefix, suffix, image
+    
+def my_collate_fn(batch,processor):
+    """
+    Collate function for the dataloader
+    Args:
+        batch (list): List of data
+    Returns:
+        dict: Dictionary of data
+    """
+    prefix = [item[0] for item in batch]
+    suffix = [item[1] for item in batch]
+    image = [item[2] for item in batch]
+    inputs = processor(text=list(prefix), images=list(image), return_tensors="pt", padding=True).to('cpu')
+    return inputs, suffix
+
+
+
+# def collate_fn(batch):
+#     """
+#     Collate function for the dataloader
+#     Args:
+#         batch (list): List of data
+#     Returns:
+#         dict: Dictionary of data
+#     """
+#     prefix = [item[0] for item in batch]
+#     suffix = [item[1] for item in batch]
+#     image = [item[2] for item in batch]
+#     inputs = processor(text=list(prefix), images=list(image), return_tensors="pt", padding=True).to('cpu')
+#     return inputs, suffix
+
